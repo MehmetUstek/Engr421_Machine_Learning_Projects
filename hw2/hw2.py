f44c8a8af11a47fa10b140e7f4bc00c7643ab6eb
MehmetUstek <mustek17@ku.edu.tr>
2021-10-30 13:17:42 +0300

    Code Cleaning

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,9 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas
 import math
-
-from scipy import linalg
 
 
 def safelog(x):
@@ -20,50 +17,46 @@ x_test = images[-5000:]
 y_test = labels[-5000:][0]
 
 K = 5
-# Estimate the mean parameters
+# Estimate the parameters
 sample_means = np.array(([np.mean(x_train[y_train == (c + 1)], axis=0) for c in range(K)]))
 print("sample_means:")
 print(sample_means)
 
 class_priors = [np.mean(y_train == (c + 1)) for c in range(K)]
 
-sample_deviations = np.array(([np.sqrt(np.mean((x_train[y_train == (c + 1)] - sample_means[c]) ** 2)) for c in range(K)]))
+sample_deviations = np.array(
+    ([np.sqrt(np.mean((x_train[y_train == (c + 1)] - sample_means[c]) ** 2)) for c in range(K)]))
 print("sample_deviations:")
 print(sample_deviations)
 
 print("class_priors:")
 print(class_priors)
+
+
 def get_score_values(x):
-    return np.sum(np.stack([-0.5 * safelog(2 * math.pi * sample_deviations[c]**2)
-                                     - 0.5 * (x - sample_means[c])**2 / sample_deviations[c]**2
-                                     + safelog(class_priors[c])
-                                     for c in range(K)]),axis=2)
-# score_values = np.array((np.sum(score_values,)))
-print("score_values")
+    return np.sum(np.stack([-0.5 * safelog(2 * math.pi * sample_deviations[c] ** 2)
+                            - 0.5 * (x - sample_means[c]) ** 2 / sample_deviations[c] ** 2
+                            + safelog(class_priors[c])
+                            for c in range(K)]), axis=2)
 
-# for i,j in score_values.items():
-#     dict[i] = j
-#     print(i)
-
-# print(score_values)
+# ---- Train
 score_values1 = get_score_values(x_train)
 score_values1 = np.transpose(score_values1)
-y_predicted = np.argmax(score_values1,axis=1) + 1
+y_predicted = np.argmax(score_values1, axis=1) + 1
 
 # Confusion Matrix
 
-confusion_matrix = pandas.crosstab(y_predicted, y_train, rownames = ['y_pred'], colnames = ['y_truth'])
-print("confusion_matrix")
+confusion_matrix = pandas.crosstab(y_predicted, y_train, rownames=['y_pred'], colnames=['y_truth'])
+print("Confusion_matrix for training:")
 print(confusion_matrix)
 
-
-# Test
+# ---- Test
 score_values2 = get_score_values(x_test)
 score_values2 = np.transpose(score_values2)
-y_predicted = np.argmax(score_values2,axis=1) + 1
+y_predicted = np.argmax(score_values2, axis=1) + 1
 
 # Confusion Matrix
 
-confusion_matrix = pandas.crosstab(y_predicted, y_test, rownames = ['y_pred'], colnames = ['y_truth'])
-print("confusion_matrix")
+confusion_matrix = pandas.crosstab(y_predicted, y_test, rownames=['y_pred'], colnames=['y_truth'])
+print("Confusion_matrix for test:")
 print(confusion_matrix)
