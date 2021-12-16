aec8fe7d30e37edb8d293fd276b26481c6935103
MehmetUstek <mustek17@ku.edu.tr>
2021-12-16 21:07:49 +0300

    Visualization - Graph

--- hw6_One_versus_all_SupporVector_classification/hw6.py
+++ hw6_One_versus_all_SupporVector_classification/hw6.py
@@ -4,16 +4,13 @@ import scipy.spatial.distance as dt
 import cvxopt as cvx
 import matplotlib.pyplot as plt
 
-def safelog(x):
-    return (np.log(x + 1e-100))
 
 images = np.genfromtxt("hw06_images.csv", delimiter = ",")
 labels = np.genfromtxt("hw06_labels.csv", delimiter = ",")
 
-# Divide the data, 30000 to training, 5000 to test
+# Divide the data, 1000 to training, 4000 to test
 X_train = images[:1000]
 y_train = labels[:1000].astype(int)
-# y_train = 2 * data_set[:,2].astype(int) - 1
 X_test = images[-4000:]
 y_test = labels[-4000:].astype(int)
 K = 5
@@ -35,18 +32,14 @@ def func(y_truth, X_given, N_given, C, s, epsilon):
     f_predicted = []
     K_train = gaussian_kernel(X_given, X_given, s)
     for k in range(1,K+1):
-        y_train_indexes = (y_truth == k).astype(int)
-        for index in range(len(y_train_indexes)):
-            if not y_train_indexes[index]:
-                y_train_indexes[index] = -1
-
-        yyK = np.matmul(y_train_indexes[:, None], y_train_indexes[None, :]) * K_train
+        y_indexes = (y_truth == k) *2 -1
+        yyK = np.matmul(y_indexes[:, None], y_indexes[None, :]) * K_train
         # print(yyK)
         P = cvx.matrix(yyK)
         q = cvx.matrix(-np.ones((N_given, 1)))
         G = cvx.matrix(np.vstack((-np.eye(N_given), np.eye(N_given))))
         h = cvx.matrix(np.vstack((np.zeros((N_given, 1)), C * np.ones((N_given, 1)))))
-        A = cvx.matrix(1.0 * y_train_indexes[None, :])
+        A = cvx.matrix(1.0 * y_indexes[None, :])
         b = cvx.matrix(0.0)
 
         # use cvxopt library to solve QP problems
@@ -67,16 +60,12 @@ def func(y_truth, X_given, N_given, C, s, epsilon):
         active_indices, = np.where(np.logical_and(alpha != 0, alpha < C))
         # Alpha points between 0 and C. As seen in the ipynb subject to.
         w0 = np.mean(
-            y_train_indexes[active_indices] * (1 - np.matmul(yyK[np.ix_(active_indices, support_indices)], alpha[support_indices])))
+            y_indexes[active_indices] * (1 - np.matmul(yyK[np.ix_(active_indices, support_indices)], alpha[support_indices])))
 
         ## Training performance
         # calculate predictions on training samples
-        f_predicted.append(np.matmul(K_train, y_train_indexes[:, None] * alpha[:, None]) + w0)
-    # print(f_predicted)
+        f_predicted.append(np.matmul(K_train, y_indexes[:, None] * alpha[:, None]) + w0)
 
-    # calculate confusion matrix
-
-        # y_predicted = 2 * (f_predicted > 0.0) - 1
     y_predicted = np.argmax(f_predicted, axis=0)
     y_predicted = y_predicted.reshape(-1)
     # print(y_predicted)
@@ -90,10 +79,10 @@ confusion_matrix = pd.crosstab(y_predicted, y_train, rownames = ['y_predicted'],
 print(confusion_matrix)
 
 
-y_predicted = func(y_test, X_test, N_test, C, s, epsilon)
-confusion_matrix = pd.crosstab(y_predicted, y_test, rownames=['y_predicted'], colnames=['y_test'])
-print("Confusion_matrix for test:")
-print(confusion_matrix)
+# y_predicted = func(y_test, X_test, N_test, C, s, epsilon)
+# confusion_matrix = pd.crosstab(y_predicted, y_test, rownames=['y_predicted'], colnames=['y_test'])
+# print("Confusion_matrix for test:")
+# print(confusion_matrix)
 
 # Accuracy score
 def accuracy_score(y_pred, y_truth):
@@ -105,26 +94,26 @@ def accuracy_score(y_pred, y_truth):
     return accuracy_score
 
 # Visualization
-C_values = [0.1,1,10,100,1000]
+C_values = [0.1,1]
 s = 10
 train_list = []
 test_list = []
 for c in C_values:
     # Train
-    y_predicted_train = func(y_train, X_train, N_train, C, s, epsilon)
+    y_predicted_train = func(y_train, X_train, N_train, c, s, epsilon)
     accuracy_score_train = accuracy_score(y_predicted_train, y_train)
     train_list.append(accuracy_score_train)
     # Test
-    y_predicted_test = func(y_test, X_test, N_test, C, s, epsilon)
+    y_predicted_test = func(y_test, X_test, N_test, c, s, epsilon)
     accuracy_score_test = accuracy_score(y_predicted_test, y_test)
     test_list.append(accuracy_score_test)
 
 plt.figure(figsize=(6, 6))
 plt.plot(C_values, train_list, "-ob", markersize=4, label='training')
 plt.plot(C_values, test_list, "-or", markersize=4, label='test')
-plt.xlabel("Pre-pruning size (P)")
-plt.ylabel("RMSE")
-plt.legend(loc='upper right')
+plt.xlabel("Regularization Parameter (C)")
+plt.ylabel("Accuracy")
+plt.legend(loc='upper left')
 plt.show()
 
 
