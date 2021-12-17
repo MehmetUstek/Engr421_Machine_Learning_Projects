494b7ebf94c71c061c089e9f29756bcb019a48fa
MehmetUstek <mustek17@ku.edu.tr>
2021-12-17 15:11:32 +0300

    Backup

--- hw6_One_versus_all_SupporVector_classification/hw6.py
+++ hw6_One_versus_all_SupporVector_classification/hw6.py
@@ -4,9 +4,8 @@ import scipy.spatial.distance as dt
 import cvxopt as cvx
 import matplotlib.pyplot as plt
 
-
-images = np.genfromtxt("hw06_images.csv", delimiter = ",")
-labels = np.genfromtxt("hw06_labels.csv", delimiter = ",")
+images = np.genfromtxt("hw06_images.csv", delimiter=",")
+labels = np.genfromtxt("hw06_labels.csv", delimiter=",")
 
 # Divide the data, 1000 to training, 4000 to test
 X_train = images[:1000]
@@ -16,24 +15,22 @@ y_test = labels[-4000:].astype(int)
 K = 5
 # get number of samples and number of features
 N_train = len(y_train)
-D_train = X_train.shape[1]
 N_test = len(y_test)
-# print(D_train)
 
 
 # define Gaussian kernel function
 def gaussian_kernel(X1, X2, s):
     D = dt.cdist(X1, X2)
-    K = np.exp(-D**2 / (2 * s**2))
-    return(K)
+    K = np.exp(-D ** 2 / (2 * s ** 2))
+    return (K)
 
 
 def func(y_truth, X_given, N_given, C, s, epsilon):
     f_predicted = []
-    K_train = gaussian_kernel(X_given, X_given, s)
-    for k in range(1,K+1):
-        y_indexes = (y_truth == k) *2 -1
-        yyK = np.matmul(y_indexes[:, None], y_indexes[None, :]) * K_train
+    K_trick = gaussian_kernel(X_given, X_given, s)
+    for k in range(1, K + 1):
+        y_indexes = (y_truth == k) * 2 - 1
+        yyK = np.matmul(y_indexes[:, None], y_indexes[None, :]) * K_trick
         # print(yyK)
         P = cvx.matrix(yyK)
         q = cvx.matrix(-np.ones((N_given, 1)))
@@ -60,29 +57,38 @@ def func(y_truth, X_given, N_given, C, s, epsilon):
         active_indices, = np.where(np.logical_and(alpha != 0, alpha < C))
         # Alpha points between 0 and C. As seen in the ipynb subject to.
         w0 = np.mean(
-            y_indexes[active_indices] * (1 - np.matmul(yyK[np.ix_(active_indices, support_indices)], alpha[support_indices])))
+            y_indexes[active_indices] * (
+                        1 - np.matmul(yyK[np.ix_(active_indices, support_indices)], alpha[support_indices])))
 
         ## Training performance
         # calculate predictions on training samples
-        f_predicted.append(np.matmul(K_train, y_indexes[:, None] * alpha[:, None]) + w0)
+        f_predicted.append(np.matmul(K_trick, y_indexes[:, None] * alpha[:, None]) + w0)
 
     y_predicted = np.argmax(f_predicted, axis=0) + 1
     y_predicted = y_predicted.reshape(-1)
-    # print(y_predicted)
     return y_predicted
+
+def predict(y_given, w0, alpha, K_trick ):
+    f_predicted = []
+    pass
+
+
+
+
 
 C = 10
 epsilon = 1e-3
 s = 10
-y_predicted_train1 = func(y_train, X_train, N_train, C, s, epsilon)
-confusion_matrix = pd.crosstab(y_predicted_train1, y_train, rownames = ['y_predicted'], colnames = ['y_train'])
-print(confusion_matrix)
+# y_predicted_train1 = func(y_train, X_train, N_train, C, s, epsilon)
+# confusion_matrix = pd.crosstab(y_predicted_train1, y_train, rownames=['y_predicted'], colnames=['y_train'])
+# print("Confusion_matrix for train:")
+# print(confusion_matrix)
 
+# y_predicted_test1 = func(y_test, X_test, N_test, C, s, epsilon)
+# confusion_matrix = pd.crosstab(y_predicted_test1, y_test, rownames=['y_predicted'], colnames=['y_test'])
+# print("Confusion_matrix for test:")
+# print(confusion_matrix)
 
-y_predicted_test1 = func(y_test, X_test, N_test, C, s, epsilon)
-confusion_matrix = pd.crosstab(y_predicted_test1, y_test, rownames=['y_predicted'], colnames=['y_test'])
-print("Confusion_matrix for test:")
-print(confusion_matrix)
 
 # Accuracy score
 def accuracy_score(y_pred, y_truth):
@@ -93,9 +99,10 @@ def accuracy_score(y_pred, y_truth):
     accuracy_score = float(score / len(y_truth))
     return accuracy_score
 
+
 # Visualization
-C_values = [0.1,1]
-s = 10
+C_values = [0.1, 1,100]
+s = 1
 train_list = []
 test_list = []
 for c in C_values:
@@ -103,10 +110,14 @@ for c in C_values:
     y_predicted_train = func(y_train, X_train, N_train, c, s, epsilon)
     accuracy_score_train = accuracy_score(y_predicted_train, y_train)
     train_list.append(accuracy_score_train)
+
     # Test
     y_predicted_test = func(y_test, X_test, N_test, c, s, epsilon)
     accuracy_score_test = accuracy_score(y_predicted_test, y_test)
     test_list.append(accuracy_score_test)
+    confusion_matrix = pd.crosstab(y_predicted_test, y_test, rownames=['y_predicted'], colnames=['y_test'])
+    print("Confusion_matrix for test:")
+    print(confusion_matrix)
 
 plt.figure(figsize=(6, 6))
 plt.plot(C_values, train_list, "-ob", markersize=4, label='training')
@@ -115,5 +126,3 @@ plt.xlabel("Regularization Parameter (C)")
 plt.ylabel("Accuracy")
 plt.legend(loc='upper left')
 plt.show()
-
-
