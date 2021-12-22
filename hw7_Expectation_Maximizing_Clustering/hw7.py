14035a65dcdba6f1bb92d904802da01e7e8384bf
MehmetUstek <mustek17@ku.edu.tr>
2021-12-23 00:37:51 +0300

    hw7 - Kmeans check

--- hw7_Expectation_Maximizing_Clustering/hw7.py
+++ hw7_Expectation_Maximizing_Clustering/hw7.py
@@ -1,54 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.spatial as spa
 
-np.random.seed(421)
-#TODO: K = ?
+initial_centroids = np.genfromtxt("hw07_initial_centroids.csv", delimiter=",")
+points = np.genfromtxt("hw07_data_set.csv", delimiter=",")
+# TODO: K = ?
 K = 5
-class_means = np.array([[2.5, 2.5], [-2.5, 2.5], [-2.5, -2.5], [2.5, -2.5], [0, 0]])
-
-class_covariances = np.array([[
-    [0.8, -0.6],
-    [-0.6, 0.8]],
-    [
-        [0.8, 0.6],
-        [0.6, 0.8]
-    ],
-    [
-        [0.8, -0.6],
-        [-0.6, 0.8]
-    ],
-    [
-        [0.8, 0.6],
-        [0.6, 0.8]
-    ],
-    [
-        [1.6, 0.0],
-        [0.0, 1.6]
-    ]
-])
-
-# number of data points for each class.
-class_sizes = np.array([50, 50, 50, 50, 100])
-
-points1 = np.random.multivariate_normal(class_means[0, :], class_covariances[0, :, :], class_sizes[0])
-points2 = np.random.multivariate_normal(class_means[1, :], class_covariances[1, :, :], class_sizes[1])
-points3 = np.random.multivariate_normal(class_means[2, :], class_covariances[2, :, :], class_sizes[2])
-points4 = np.random.multivariate_normal(class_means[3, :], class_covariances[3, :, :], class_sizes[3])
-points5 = np.random.multivariate_normal(class_means[4, :], class_covariances[4, :, :], class_sizes[4])
-
-points = np.vstack((points1, points2, points3, points4, points5))
-
-# generating the labels.
-y = np.concatenate((np.repeat(1, class_sizes[0]), np.repeat(2, class_sizes[1]), np.repeat(3, class_sizes[2]), np.repeat(4,class_sizes[3]), np.repeat(5, class_sizes[4])))
-
 x1 = points[:, 0]
 x2 = points[:, 1]
 # Plotting Data
 plt.figure(figsize=(8, 8))
-plt.plot(points[:, 0], points[:, 1], "k.", markersize=12)
+plt.plot(x1, x2, "k.", markersize=12)
 plt.xlabel("$x_1$")
 plt.ylabel("$x_2$")
 plt.show()
+
+
+# Step 2
+def update_centroids(memberships, X):
+    if memberships is None:
+        # This is the first iteration.
+        # initialize centroids
+        centroids = initial_centroids
+    else:
+        # update centroids
+        # Calculate sample mean for K = 0, K= 1, K = 2, K = 3 and done.
+        # return centroids array.
+        centroids = np.vstack([np.mean(X[memberships == k,:], axis = 0) for k in range(K)])
+    return(centroids)
+# Step 1
+def update_memberships(centroids, X):
+    # calculate distances between centroids and data points
+    # Distance between all centroids and data points.
+    # D matrix is KxN.
+    D = spa.distance_matrix(centroids, X)
+    # find the nearest centroid for each data point
+    # Nearest centroid.
+    memberships = np.argmin(D, axis = 0)
+    return(memberships)
 
 def plot_current_state(centroids, memberships, X):
     cluster_colors = np.array(["#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a", "#b15928",
@@ -64,3 +53,30 @@ def plot_current_state(centroids, memberships, X):
                  markerfacecolor = cluster_colors[c], markeredgecolor = "black")
     plt.xlabel("x1")
     plt.ylabel("x2")
+
+centroids = None
+memberships = None
+iteration = 1
+while True:
+    print("Iteration#{}:".format(iteration))
+
+    old_centroids = centroids
+    centroids = update_centroids(memberships, points)
+    if np.alltrue(centroids == old_centroids):
+        break
+    else:
+        plt.figure(figsize = (12, 6))
+        plt.subplot(1, 2, 1)
+        plot_current_state(centroids, memberships, points)
+
+    old_memberships = memberships
+    memberships = update_memberships(centroids, points)
+    if np.alltrue(memberships == old_memberships):
+        plt.show()
+        break
+    else:
+        plt.subplot(1, 2, 2)
+        plot_current_state(centroids, memberships, points)
+        plt.show()
+
+    iteration = iteration + 1
