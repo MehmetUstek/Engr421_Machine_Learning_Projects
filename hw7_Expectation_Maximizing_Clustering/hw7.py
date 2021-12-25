27d2f13baaea302e6fe0cd6fa93427bfd876a11e
MehmetUstek <mustek17@ku.edu.tr>
2021-12-25 19:32:08 +0300

    hw7 - EM - First Step Done

--- hw7_Expectation_Maximizing_Clustering/hw7.py
+++ hw7_Expectation_Maximizing_Clustering/hw7.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.spatial as spa
+from scipy.stats import multivariate_normal
 
 initial_centroids = np.genfromtxt("hw07_initial_centroids.csv", delimiter=",")
 points = np.genfromtxt("hw07_data_set.csv", delimiter=",")
@@ -15,8 +16,40 @@ plt.xlabel("$x_1$")
 plt.ylabel("$x_2$")
 plt.show()
 
+class_means = np.array([[2.5, 2.5], [-2.5, 2.5], [-2.5, -2.5],[2.5, -2.5],[0.0, 0.0]])
 
-# Step 2
+class_covariances = np.array([[
+    [0.8, -0.6],
+    [-0.6, 0.8]],
+    [
+        [0.8, 0.6],
+        [0.6, 0.8]
+    ],
+    [
+        [0.8, -0.6],
+        [-0.6, 0.8]
+    ],
+    [
+        [0.8, 0.6],
+        [0.6, 0.8]
+    ],
+    [
+        [1.6, 0.0],
+        [0.0, 1.6]
+    ]
+])
+
+priors = np.array([50, 50, 50, 50, 100])
+
+points1 = np.random.multivariate_normal(class_means[0, :], class_covariances[0, :, :], priors[0])
+points2 = np.random.multivariate_normal(class_means[1, :], class_covariances[1, :, :], priors[1])
+points3 = np.random.multivariate_normal(class_means[2, :], class_covariances[2, :, :], priors[2])
+points4 = np.random.multivariate_normal(class_means[3, :], class_covariances[3, :, :], priors[3])
+points5 = np.random.multivariate_normal(class_means[4, :], class_covariances[4, :, :], priors[4])
+
+probabilities = [1/6, 1/6, 1/6, 1/6, 1/3]
+
+# Step 2: M Step
 def update_centroids(memberships, X):
     if memberships is None:
         # This is the first iteration.
@@ -26,9 +59,10 @@ def update_centroids(memberships, X):
         # update centroids
         # Calculate sample mean for K = 0, K= 1, K = 2, K = 3 and done.
         # return centroids array.
+
         centroids = np.vstack([np.mean(X[memberships == k,:], axis = 0) for k in range(K)])
     return(centroids)
-# Step 1
+# Step 1: E Step
 def update_memberships(centroids, X):
     # calculate distances between centroids and data points
     # Distance between all centroids and data points.
@@ -38,6 +72,26 @@ def update_memberships(centroids, X):
     # Nearest centroid.
     memberships = np.argmin(D, axis = 0)
     return(memberships)
+def e_step(t, k):
+    denominator = 0.0
+    iteration_gaussian = multivariate_normal.pdf(points, mean= centroids[t, :], cov= class_covariances[t, :, :])
+    numerator = iteration_gaussian * probabilities[k]
+    for c in range(K):
+        # current_point = np.random.multivariate_normal(centroids[t, :], class_covariances[t, :, :], priors[c])
+        current_gaussian = multivariate_normal.pdf(points, mean= centroids[t, :], cov= class_covariances[t, :, :])
+        denominator += current_gaussian * probabilities[c]
+    return numerator / denominator
+
+
+def m_step(memberships, X, class_covariances, probabilities):
+    if memberships is None:
+        centroids = initial_centroids
+    else:
+        centroids = np.vstack([np.mean(X[memberships == k, :], axis=0) for k in range(K)])
+        class_covariances = np.vstack([np.mean((X[memberships == k, :] - centroids[k]) * np.transpose(X[memberships == k, :] - centroids[k]), axis=0) for k in range(K)])
+        probabilities = np.vstack([np.sum(X[memberships == k, :], axis=0)/ K for k in range(K)])
+    return (centroids, class_covariances, probabilities)
+
 
 def plot_current_state(centroids, memberships, X):
     cluster_colors = np.array(["#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a", "#b15928",
@@ -61,7 +115,8 @@ while True:
     print("Iteration#{}:".format(iteration))
 
     old_centroids = centroids
-    centroids = update_centroids(memberships, points)
+    # centroids = update_centroids(memberships, points)
+    centroids, class_covariances, probabilities = m_step(memberships, points, class_covariances, probabilities)
     if np.alltrue(centroids == old_centroids):
         break
     else:
@@ -70,7 +125,11 @@ while True:
         plot_current_state(centroids, memberships, points)
 
     old_memberships = memberships
-    memberships = update_memberships(centroids, points)
+    # memberships = update_memberships(centroids, points)
+    membership_probs = []
+    for k in range(K):
+        membership_probs.append(e_step(iteration, k))
+    memberships = np.argmax(membership_probs, axis= 0)
     if np.alltrue(memberships == old_memberships):
         plt.show()
         break
