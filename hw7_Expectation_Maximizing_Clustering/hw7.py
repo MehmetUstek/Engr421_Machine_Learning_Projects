e4abf8ad6b74e08eff29ecd1f19b971785fd49fe
MehmetUstek <mustek17@ku.edu.tr>
2021-12-27 19:25:13 +0300

    Code Cleaning -- Memberships updated

--- hw7_Expectation_Maximizing_Clustering/hw7.py
+++ hw7_Expectation_Maximizing_Clustering/hw7.py
@@ -14,56 +14,53 @@ plt.figure(figsize=(8, 8))
 plt.plot(x1, x2, "k.", markersize=12)
 plt.xlabel("$x_1$")
 plt.ylabel("$x_2$")
+plt.show()
 N = points.shape[0]
-# plt.show()
 
-# Step 1: E Step
+
 def update_memberships(centroids, X):
-    # calculate distances between centroids and data points
-    # Distance between all centroids and data points.
-    # D matrix is KxN.
     D = spa.distance_matrix(centroids, X)
-    # find the nearest centroid for each data point
-    # Nearest centroid.
-    memberships = np.argmin(D, axis = 0)
-    # class_covariances =
-    return(memberships)
-def e_step(t, Fi):
+    memberships = np.argmin(D, axis=0)
+    return (memberships)
+
+
+def e_step(Fi):
     centroids = Fi[0]
     class_covariances = Fi[1]
     probabilities = Fi[2]
 
     denominator = 0.0
-    numerator = np.vstack([multivariate_normal.pdf(points, mean= centroids[k, :], cov= class_covariances[k, :, :]) * probabilities[k] for k in range(K)])
+    numerator = np.vstack(
+        [multivariate_normal.pdf(points, mean=centroids[k, :], cov=class_covariances[k, :, :]) * probabilities[k] for k
+         in range(K)])
     for c in range(K):
-        current_gaussian = multivariate_normal.pdf(points, mean= centroids[c, :], cov= class_covariances[c, :, :])
+        current_gaussian = multivariate_normal.pdf(points, mean=centroids[c, :], cov=class_covariances[c, :, :])
         denominator += current_gaussian * probabilities[c]
-    probs = (numerator / denominator).T
-    return probs
+    return (numerator / denominator).T
 
 
-def m_step(X, memberships_probs):
+def m_step(X, memberships_probabilities):
     denom = 0.0
-    centroids = np.zeros(shape=(5,2))
+    centroids = np.zeros(shape=(5, 2))
     for i in range(N):
-        current_point = memberships_probs[i]
+        current_point = memberships_probabilities[i]
         for k in range(K):
             centroids[k] += current_point[k] * X[i]
         denom += np.array([np.sum(current_point[k]) for k in range(K)])
     centroids = (centroids.T / denom).T
     denom = 0.0
-    class_covariances = np.zeros(shape=(5,2,2))
+    class_covariances = np.zeros(shape=(5, 2, 2))
     for i in range(N):
-        current_point = memberships_probs[i]
+        current_point = memberships_probabilities[i]
         for k in range(K):
             class_covariances[k] += current_point[k] * (X[i] - centroids[k])[None, :] * (X[i] - centroids[k])[:,
                                                                                         None]
         denom += np.array([np.sum(current_point[k]) for k in range(K)])
     class_covariances = (class_covariances.T / denom).T
 
-    probabilities = [0,0,0,0,0]
+    probabilities = [0, 0, 0, 0, 0]
     for i in range(N):
-        current_point = memberships_probs[i]
+        current_point = memberships_probabilities[i]
         probabilities += np.array([np.sum(current_point[k]) for k in range(K)])
     probabilities = probabilities / N
 
@@ -71,35 +68,36 @@ def m_step(X, memberships_probs):
     return (Fi)
 
 
-
 def plot_current_state(centroids, memberships, X):
     cluster_colors = np.array(["#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a", "#b15928",
                                "#a6cee3", "#b2df8a", "#fb9a99", "#fdbf6f", "#cab2d6", "#ffff99"])
     if memberships is None:
-        plt.plot(x1, x2, ".", markersize = 10, color = "black")
+        plt.plot(x1, x2, ".", markersize=10, color="black")
     else:
         for c in range(K):
-            plt.plot(X[memberships == c, 0], X[memberships == c, 1], ".", markersize = 10,
-                     color = cluster_colors[c])
+            plt.plot(X[memberships == c, 0], X[memberships == c, 1], ".", markersize=10,
+                     color=cluster_colors[c])
     for c in range(K):
-        plt.plot(centroids[c, 0], centroids[c, 1], "s", markersize = 12,
-                 markerfacecolor = cluster_colors[c], markeredgecolor = "black")
+        plt.plot(centroids[c, 0], centroids[c, 1], "s", markersize=12,
+                 markerfacecolor=cluster_colors[c], markeredgecolor="black")
     plt.xlabel("x1")
     plt.ylabel("x2")
+
 
 # centroids = None
 # memberships = None
 # class_covariances = None
 # probabilities = None
-# memberships_probs = None
+# memberships_probabilities = None
 
 centroids = initial_centroids
 memberships = update_memberships(centroids, points)
 class_sizes = np.array(np.bincount(memberships))
 class_covariances = np.array([
-    np.mat(points[memberships == k, :] - centroids[k, :]).T * np.mat(points[memberships == k, :] - centroids[k, :]) / class_sizes[k]
+    np.mat(points[memberships == k, :] - centroids[k, :]).T * np.mat(points[memberships == k, :] - centroids[k, :]) /
+    class_sizes[k]
     for k in range(K)])
-probabilities = np.array(np.bincount(memberships) / N)
+probabilities = np.array(class_sizes / N)
 
 iteration = 1
 Fi = []
@@ -107,15 +105,16 @@ for i in range(100):
     print("Iteration#{}:".format(iteration))
 
     Fi = (centroids, class_covariances, probabilities)
-    memberships_probs = e_step(iteration, Fi)
+    membership_probabilities = e_step(Fi)
 
-    Fi= m_step(points, memberships_probs)
+    Fi = m_step(points, membership_probabilities)
     centroids = Fi[0]
     class_covariances = Fi[1]
     probabilities = Fi[2]
 
     iteration = iteration + 1
 print(centroids)
-plt.subplot(1, 2, 2)
-# plot_current_state(centroids, class_covariances, memberships, points)
+memberships = np.argmax(membership_probabilities, axis=1)
+plt.subplot(1,2,2)
+plot_current_state(centroids, memberships, points)
 plt.show()
